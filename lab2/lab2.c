54563f2d2e0a974cb12193702f46edd8a8bdd602
areedjones <a.reedjones@gmail.com>
2013-10-07 19:08:29 -0500

    initialized TIM1, still need to call fake function calls using TIM1 interrupts

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -108,21 +108,21 @@ void TIM2_Config(void)
   /* TIM2, TIM3 and TIM4 enable counters */
   TIM_Cmd(TIM2, ENABLE);
 }
-void TIM5_Config(void)
+void TIM1_Config(void)
 { 
   TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
   TIM_OCInitTypeDef  TIM_OCInitStructure;
   NVIC_InitTypeDef  NVIC_InitStructure;
 
   /* Enable TIM2, TIM3 and TIM4 clocks */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
 
   /* TIM2 configuration */
   TIM_TimeBaseStructure.TIM_Period = 4800 - 1;       
   TIM_TimeBaseStructure.TIM_Prescaler = ((SystemCoreClock/1200) - 1);
   TIM_TimeBaseStructure.TIM_ClockDivision = 0x0;    
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  
-  TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+  TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
   //TIM_OCStructInit(&TIM_OCInitStructure);
   
   /* Output Compare Timing Mode configuration: Channel1 */
@@ -130,37 +130,37 @@ void TIM5_Config(void)
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_Pulse = T2_CCR1_Val;
   TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;
-  TIM_OC1Init(TIM5, &TIM_OCInitStructure);
+  TIM_OC1Init(TIM1, &TIM_OCInitStructure);
 
   TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Disable);
 
   TIM_OCInitStructure.TIM_Pulse = T2_CCR2_Val;
-  TIM_OC2Init(TIM5, &TIM_OCInitStructure);
+  TIM_OC2Init(TIM1, &TIM_OCInitStructure);
 
-  TIM_OC2PreloadConfig(TIM5, TIM_OCPreload_Disable);
+  TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Disable);
 
   /* Immediate load of TIM2,TIM3 and TIM4 Precaler values */
   //TIM_PrescalerConfig(TIM2, ((SystemCoreClock/1200) - 1), TIM_PSCReloadMode_Immediate);
   //TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Disable);
 
   /* Clear TIM2, TIM3 and TIM4 update pending flags */
-  TIM_ClearFlag(TIM5, TIM_FLAG_Update);
+  TIM_ClearFlag(TIM1, TIM_FLAG_Update);
 
   /* Configure two bits for preemption priority */
   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 
   /* Enable the TIM2 Interrupt */
-  //NVIC_InitStructure.NVIC_IRQChannel = TIM7_IRQn;
+//  NVIC_InitStructure.NVIC_IRQChannel = TIM7_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
 
   /* Enable TIM2, TIM3 and TIM4 Update interrupts */
-  TIM_ITConfig(TIM5, TIM_IT_CC1 | TIM_IT_CC2, ENABLE);
+  TIM_ITConfig(TIM1, TIM_IT_CC1 | TIM_IT_CC2, ENABLE);
 
   /* TIM2, TIM3 and TIM4 enable counters */
-  TIM_Cmd(TIM5, ENABLE);
+  TIM_Cmd(TIM1, ENABLE);
 }
 
 
@@ -229,7 +229,8 @@ void TIM2_IRQHandler(void)
     TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
 
     /* LED3 toggling with frequency = 219.7 Hz */
-    ledToggle();
+     ledToggle();
+     //redLedToggle();
  // redLedToggle();
  // motorSwitch();
 
@@ -239,7 +240,7 @@ void TIM2_IRQHandler(void)
   if(TIM_GetITStatus(TIM2, TIM_IT_CC2) != RESET)
   {
     TIM_ClearITPendingBit(TIM2, TIM_IT_CC2);
-    //redLedToggle();
+    redLedToggle();
 
     capture = TIM_GetCapture2(TIM2);
     TIM_SetCompare2(TIM2, (capture + T2_CCR2_Val) % TIM2_Period);
@@ -250,32 +251,7 @@ void TIM2_IRQHandler(void)
   //redLedToggle();
 //  motorSwitch();
 }
-void TIM3_IRQHandler(void){
-  int T3_CCR1_Val = 332;
-  int T3_CCR2_Val = 332;
-  int TIM3_Period = 665;
-  if (TIM_GetITStatus(TIM3, TIM_IT_CC1) != RESET)
-  {
-    TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
-
-    /* LED3 toggling with frequency = 219.7 Hz */
-     redLedToggle();
-     //redLedToggle();
- // redLedToggle();
- // motorSwitch();
-
-    capture = TIM_GetCapture1(TIM3);
-    TIM_SetCompare1(TIM3, (capture + T3_CCR1_Val) % TIM3_Period);
-  }
-  if(TIM_GetITStatus(TIM3, TIM_IT_CC2) != RESET)
-  {
-    TIM_ClearITPendingBit(TIM3, TIM_IT_CC2);
-    //redLedToggle();
-
-    capture = TIM_GetCapture2(TIM3);
-    TIM_SetCompare2(TIM3, (capture + T3_CCR2_Val) % TIM3_Period);
-  }
-
+void TIM5_IRQHandler(void){
 }
 
 
@@ -435,7 +411,6 @@ void motorHandler(void)
     TIM3 Channel3 duty cycle = (TIM3_CCR3/ TIM3_ARR)* 100 = 25%
     TIM3 Channel4 duty cycle = (TIM3_CCR4/ TIM3_ARR)* 100 = 12.5%
   ----------------------------------------------------------------------- */
-  NVIC_InitTypeDef  NVIC_InitStructure;
   /* Compute the prescaler value */
   PrescalerValue = (uint16_t) (SystemCoreClock / 24000000) - 1;
   /* Time base configuration */
@@ -447,18 +422,7 @@ void motorHandler(void)
   TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
   TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
 
-
-  /* Output Compare Timing Mode configuration: Channel1 */
-  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Toggle;
-  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-  TIM_OCInitStructure.TIM_Pulse = 332;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;
-  TIM_OC1Init(TIM5, &TIM_OCInitStructure);
-  TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Disable);
-
-
-
-  /* PWM1 Mode configuration: Channel3 */
+  /* PWM1 Mode configuration: Channel1 */
   TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_Pulse = 0; // CCR1_Val;
@@ -469,7 +433,7 @@ void motorHandler(void)
   TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
   
 
-  /* PWM1 Mode configuration: Channel4 */
+  /* PWM1 Mode configuration: Channel2 */
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_Pulse = 0; // CCR2_Val;
 
@@ -495,24 +459,6 @@ void motorHandler(void)
   TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
   TIM_ARRPreloadConfig(TIM3, ENABLE);
-
-
-    /* Clear TIM2, TIM3 and TIM4 update pending flags */
-  TIM_ClearFlag(TIM3, TIM_FLAG_Update);
-
-  /* Configure two bits for preemption priority */
-  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-
-  /* Enable the TIM2 Interrupt */
-  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
-
-  /* Enable TIM2, TIM3 and TIM4 Update interrupts */
-  TIM_ITConfig(TIM3, TIM_IT_CC1 | TIM_IT_CC2, ENABLE);
-
 
   /* TIM3 enable counter */
   TIM_Cmd(TIM3, ENABLE);
